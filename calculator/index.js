044668b158059813ea751ddc4491119cd935d023
Safad mt <116543401+safadmt@users.noreply.github.com>
2022-12-18 13:34:21 +0530

    Add files via upload

---
+++ calculator/index.js
@@ -0,0 +1,52 @@
+let inputfirstvalue
+let inputvalue = document.getElementById('inputEl')
+let arithmaticoperator = ""
+let inputsecondvalue = ""
+let result
+ 
+
+function buttonclick(value) {
+   inputvalue.innerHTML += value 
+}
+
+
+function evaluatebtn(logical){
+    arithmaticoperator = logical
+    inputfirstvalue = inputvalue.innerHTML
+    document.getElementById('calc_input_div').innerHTML = inputfirstvalue
+    inputvalue.innerHTML = ''
+    console.log(inputfirstvalue)
+}
+
+function clearinput() {
+    console.log('inupt value')
+    document.getElementById('calc_input_div').innerHTML = ""
+    inputvalue.innerHTML = ""
+    inputfirstvalue = ""
+}
+
+function equalTo(){
+   if(inputfirstvalue == ""){
+
+   }
+   else if(arithmaticoperator === '*'){
+    result = parseFloat(inputfirstvalue) * parseFloat(inputvalue.innerHTML)
+        result.toFixed(2)
+        inputvalue.innerHTML = result
+   }else if(arithmaticoperator === '-'){
+    result = parseFloat(inputfirstvalue) - parseFloat(inputvalue.innerHTML)
+        result.toFixed(2)
+        inputvalue.innerHTML = result
+   }else if(arithmaticoperator === '/'){
+    result = parseFloat(inputfirstvalue) / parseFloat(inputvalue.innerHTML)
+        result.toFixed(2)
+        inputvalue.innerHTML = result
+   }else if(arithmaticoperator === "+"){
+    result = parseFloat(inputfirstvalue) + parseFloat(inputvalue.innerHTML)
+        result.toFixed(2)
+        inputvalue.innerHTML = result
+   }
+   else{
+    inputvalue.innerHTML= "Please Select operator"
+   }
+}
